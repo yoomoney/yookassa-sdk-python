5a62bbeb0b17c9a11a687007f176daa59a70546f
Антон Н. Николаев <nikolaevanym@yoomoney.ru>
2021-10-20 10:29:49 +0300

    Добавлена поддержка параметра on_behalf_of для метода /me

--- src/yookassa/settings.py
+++ src/yookassa/settings.py
@@ -10,9 +10,16 @@ class Settings:
         self.client = ApiClient()
 
     @classmethod
-    def get_account_settings(cls):
+    def get_account_settings(cls, params=None):
+        """
+        Shop Info
+
+        :param params: (dict | None) Параметры поиска.
+            В настоящее время доступен только {'on_behalf_of': account_id}
+        :return: dict
+        """
         instance = cls()
         path = cls.base_path
 
-        response = instance.client.request(HttpVerb.GET, path)
+        response = instance.client.request(HttpVerb.GET, path, params)
         return response
